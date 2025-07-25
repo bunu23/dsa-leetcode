ba9295ffe8050af6ec1e32444df617dd87d6ca1a
bunu23 <94806809+bunu23@users.noreply.github.com>
2025-07-25 11:16:36 -0400

    feat: add Java array implementation

---
+++ dsa/array/java-array-examples/01-basic/ArrayDeclaration.java
@@ -0,0 +1,9 @@
+public class ArrayDeclaration{
+
+  public static void main(String[] args){
+
+    // declaring an array of integers
+    int[] numbers;
+    System.out.println("Arrays declared successfully.");
+  }
+}
