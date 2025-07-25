22d340c419d1398ba851a029d92d48b90be3be56
bunu23 <94806809+bunu23@users.noreply.github.com>
2025-07-25 11:22:37 -0400

    array initialization in java

---
+++ dsa/array/java-array-examples/01-basic/ArrayInitialization.java
@@ -0,0 +1,19 @@
+public class ArrayInitialization{
+
+   public static void main(String[] args){
+
+        // declaring and initializing an array
+        int[] numbers={10,20,30};
+        System.out.println("Array initialized with values: ");
+        for(int num: numbers){
+            System.out.print(num + " ");
+        }
+    }
+
+}
+
+/*
+Output:
+Array initialized with values: 
+10 20 30 
+*/
